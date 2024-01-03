16837f05f4caa4450e6bae824c33d12a0ec63fb2
El-Lloros <dbarrosoc99@gmail.com>
2024-01-03 23:19:20 +0100

    Comentarios en AprobarVinos

--- AprobarVinos.cpp
+++ AprobarVinos.cpp
@@ -26,32 +26,57 @@ análisis de casos, acciones realizadas para cada caso...)
 
 */
 void solucion( 
-//Datos del problema
+//Datos del problema (datos que nos da el problema, en este caso la entrada)
 const tInfoTema info_temas[], unsigned int num_temas, float tiempo_disponible,
 //Solucion Parcial
-int k,
-float notaAcumulada1,
-float notaAcumulada2,
-float tiempoConsumido,
+int k,//Tema que vamos a decidir si vamos a estudiar o no [0,numtemas-1]
+float notaAcumulada1,//Nota que llevamos en la parte 1 antes de decidir que hacer con el tema k
+float notaAcumulada2,//Nota que llevamos en la parte 2 antes de decidir que hacer con el tema k
+float tiempoConsumido,//Tiempo que hemos consumido antes de decidir que hacer con el tema k
 //Marcaje
-float &notaMaxima,
+float &notaMaxima,//Nota maxima que hemos llegado a conseguir despues de decidir que hacer con todos los temas
+                  // esta nota solo se actualiza si hemos aprobado  
 //Poda
-float notaRestante1,
-float notaRestante2
+float notaRestante1,//Nota que aun se puede obtener  (Si estudias todos los teamas que quedan)
+                    // en la Primera parte
+float notaRestante2//Nota que aun se puede obtener  (Si estudias todos los teamas que quedan)
+                    // en la segunda parte
 ){
     //Caso Base
-    if(num_temas==k){
+    if(num_temas==k){//Ya hemos decidido que hacer con todos lo temas
          notaMaxima=(notaAcumulada1+notaAcumulada2)/2;
+         //si hemos llegado aqui es la nota maxima asi que actualizamos
+         //la nota con la media de las dos partes
          return;
     }
-
+    /*
+        En el caso recursivo solo hay dos opciones:
+        1.- Estudiamos el tema k
+        2.- No estudiamos el tema k
+    */
     //Estudiamos tema k
+    /*
+        Si estudiamos el tema K tenemos que comprobar si nos da tiempo a estudiarlo
+        si no nos da tiempo nos saltamos esta opción
+    */
     if(tiempoConsumido+info_temas[k].tiempo_req<=tiempo_disponible){
         solucion(info_temas,num_temas,tiempo_disponible,k+1,notaAcumulada1+info_temas[k].nota_prim,
         notaAcumulada2+info_temas[k].nota_sec,tiempoConsumido+info_temas[k].tiempo_req,notaMaxima,
         notaRestante1-info_temas[k].nota_prim,notaRestante2-info_temas[k].nota_sec);
     }
+    /*
+        Su llegamos aqui es por una de estas dos razones:
+        1.- Ya hemos visto que pasaría si nos estudiamos K 
+            y ahora vamos a comprobar el resultado si no nos estudiamos K
+        2.- No nos da tiempo a estudiar el tema K
 
+        ¿Cuándo podemos NO estudiar el tema K?
+        Cuando incluso si no obtenemos la nota de K aun tenemos posibilidades 
+        de aprobar si nos estudiamos los temas que nos quedan despues de K
+        Para eso comprobamso que la nota que llevamos ahora (notaAcumulada) + la nota que aun podemos conseguir (notaRestante-nota del tema K (ya que no lo vamos a estudiar)) es >=5
+        PODA:
+        Ademas tenemos que comprobar (Segundo IF) que tenemos posibilidades de mejorar la nota maxima ya obtenida
+    */
     //No estudiamos k
     if((notaAcumulada1+notaRestante1-info_temas[k].nota_prim>=5)&&(notaAcumulada2+notaRestante2-info_temas[k].nota_sec>=5)){
         if((notaAcumulada1+notaRestante1-info_temas[k].nota_prim+notaAcumulada2+notaRestante2-info_temas[k].nota_sec)/2>notaMaxima){
@@ -61,6 +86,7 @@ float notaRestante2
         }
 
     }
+    //Si llegamos aqui ya no hay mas opciones asi que cortamos 
 
     return;
 
@@ -76,14 +102,15 @@ float mejor_puntuacion(const tInfoTema info_temas[], unsigned int num_temas, flo
 	  /* ESTA FUNCIÓN SERÁ EL PUNTO DE ENTRADA AL ALGORITMO. Puedes, después, utilizar las 
 	     funciones auxiliares que consideres oportuno, pero debes incluir la justificación para ello
 		 en el comentario de descripción detallado del diseño de dicho algoritmo */
-         float notaRestante1=0;
-         float notaRestante2=0;
+         float notaRestante1=0;//Nota que aun se puede obtener (Si estudias todos los teamas que quedan) en la Primera parte
+         float notaRestante2=0;//Nota que aun se puede obtener (Si estudias todos los teamas que quedan) en la Segunda parte
          for(int i=0;i<num_temas;i++){
+            //Al empezar tienes todos los puntos disponibles (No hemos descartado temas aun)
             notaRestante1+=info_temas[i].nota_prim;
             notaRestante2+=info_temas[i].nota_sec;
          }
          float notaMaxima;
-         notaMaxima=-1;
+         notaMaxima=-1;//Si no se puede aprobar, nos dicen que tenemos que devolver -1
          solucion(info_temas,num_temas,tiempo_disponible,0,0,0,0,notaMaxima,notaRestante1,notaRestante2);
 		 return notaMaxima;
 }
